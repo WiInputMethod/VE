26c7e3e41ee291dca5987a47b61b601684d025d2
bahaodangpu <18845091603@163.com>
2016-05-11 22:05:55 +0800

    设置-添加自定义候选词大小选项

---
+++ code/app/src/main/java/com/hit/wi/ve/view/TextSizePreference.java
@@ -0,0 +1,49 @@
+package com.hit.wi.ve.view;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import com.hit.wi.util.CommonFuncs;
+import com.hit.wi.ve.R;
+import com.hit.wi.ve.values.Global;
+
+/**
+ * Created by daofa on 2016/5/11.
+ */
+public class TextSizePreference extends DialogPreference implements
+        SeekBar.OnSeekBarChangeListener {
+
+    private TextView tv_sample;
+
+    public TextSizePreference(final Context context, final AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+    @Override
+    protected void onBindDialogView(View view) {
+        final SeekBar textsizeBar = (SeekBar) view.findViewById(R.id.textsizeBar);
+        tv_sample = (TextView) view.findViewById(R.id.tv_textsize_sample);
+        textsizeBar.setOnSeekBarChangeListener(this);
+        textsizeBar.setProgress(getSharedPreferences().getInt(getKey(),0));
+        super.onBindDialogView(view);
+
+    }
+    public final void onProgressChanged(final SeekBar seekBar,
+                                        final int progress, final boolean fromUser) {
+        tv_sample.setTextSize(progress);
+        Global.textsizeFactor =  ((float) progress)/50;
+        Global.textsizeFactor = Math.max(0f, Math.min(Global.textsizeFactor, 2f));
+    }
+
+
+    public final void onStartTrackingTouch(final SeekBar seekBar) {
+    }
+
+    public final void onStopTrackingTouch(final SeekBar seekBar) {
+    }
+
+}
